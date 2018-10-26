03200884b2a007631b40abcbc53d5de244ee8fab
Dominic White <dewhite4@gmail.com>
2018-10-25 20:06:57 -0400

    Inject Mapbox key as environment variable

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import Vue from 'vue';
+import Mapbox from 'mapbox-gl-vue';
+
+console.log(__MAPBOX_KEY__);
 
 const app = new Vue({
     el: '#app',
     data: {
-        title: 'NoVaEats'
+        title: 'NoVaEats',
+        mapboxKey: __MAPBOX_KEY__
+    },
+    components: {
+        Mapbox
     }
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,26 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    devServer: {
-        publicPath: "/",
-        contentBase: './public',
-        hot: true
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-        })
-    ],
-    resolve: {
-        alias: {
-            vue: 'vue/dist/vue.js'
+module.exports = env => {
+    return {
+        devServer: {
+            publicPath: "/",
+            contentBase: './public',
+            hot: true
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin(),
+            new HtmlWebpackPlugin({
+                template: './src/index.html',
+            }),
+            new webpack.DefinePlugin({
+                __MAPBOX_KEY__: JSON.stringify(env.MAPBOX)
+            })
+        ],
+        resolve: {
+            alias: {
+                vue: 'vue/dist/vue.js'
+            }
         }
-    }
+    };
 };
